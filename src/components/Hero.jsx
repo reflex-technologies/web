8a84fbba84b47fb31736dae35b36ac473675b140
Tom-Bartie <tjb141@pitt.edu>
2025-01-26 14:21:51 -0500

    Initial Commit
    Using Tailwind CSS and React.  Currently using a dark mode color scheme but can change to light mode in tailwiond.config.js color defines.  Also using some template svg files for logo which can easily be replaced once a new logo is generated.

---
+++ src/components/Hero.jsx
@@ -0,0 +1,39 @@
+import { curve, heroBackground, robot } from "../assets";
+import Section from "./Section";
+
+const Hero = () => {
+
+  return (
+    <Section
+      className="pt-[12rem] -mt-[5.25rem]"
+      crosses
+      crossesOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
+    >
+      <div className="container relative">
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
+          <h1 className="h1 mb-6">
+            Explore the Possibilities of ... with {` `}
+            <span className="inline-block relative">
+              Reflex{" "}
+              <img
+                src={curve}
+                className="absolute top-full left-0 w-full xl:-mt-2"
+                width={624}
+                height={28}
+                alt="Curve"
+              />
+            </span>
+          </h1>
+          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+            Insert more text here later on.
+          </p>
+          </div>
+        </div>
+   
+    </Section>
+  );
+};
+
+export default Hero;
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter as Router } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+)
