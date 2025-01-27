547952799eb49fa15e595cf21fd332f2c3e85990
Tom-Bartie <tjb141@pitt.edu>
2025-01-26 20:28:25 -0500

    Adding image to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import { curve, heroBackground, robot } from "../assets";
 import Section from "./Section";
-
+import Button from "./Button"
 const Hero = () => {
 
   return (
@@ -14,9 +14,9 @@ const Hero = () => {
       <div className="container relative">
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of ... with {` `}
+            Explore the Possibilities with {` `}
             <span className="inline-block relative">
-              Reflex{" "}
+              Reflex Technologies{" "}
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
@@ -29,6 +29,28 @@ const Hero = () => {
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
             Insert more text here later on.
           </p>
+          <Button href="/demo" white>
+            Watch Demo
+          </Button>
+          </div>
+          <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+            <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+              <div className="relative bg-n-8 rounded-[1rem]">
+                <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+                <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                  <img
+                    src={robot}
+                    className="w-full scale-[1.7] translate-y-[8%] md:scale-[1]   md:-translate-y-[10%] lg:-translate-y-[23%]"
+                    width={1024}
+                    height={490}
+                    alt="AI"
+                  />
+                </div>
+              </div>
+            </div>
+              <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+                <img src={heroBackground} className="w-full" width={1440} height={1800} alt="hero"></img>
+              </div>
           </div>
         </div>
    
