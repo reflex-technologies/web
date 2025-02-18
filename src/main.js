373b9e9e13f7d19fefb007b853793c51eca2578a
itIsBrando <mooballygoobally@gmail.com>
2025-02-18 00:18:23 -0500

    Made CSS adaptive

--- src/main.js
+++ src/main.js
@@ -1,9 +1,37 @@
-function clickTabbar(event, name) {
-    const buttons = document.getElementsByClassName("tab-bar-active");
+const tabs = document.getElementsByClassName('tab-bar-button');
+const tabTitles = ['Home', 'Features', 'Why'];
 
-    for(let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('tab-bar-active');
+function clickTabbar(target, name, scroll = true) {
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove('tab-bar-active');
     }
 
-    event.target.classList.add('tab-bar-active');
+    target.classList.add('tab-bar-active');
+
+    if (scroll)
+        document.getElementById(name).scrollIntoView({ behavior: 'smooth', block: 'center' });
 }
+
+
+const contentDiv = document.getElementById('content');
+let ticking = false;
+
+
+contentDiv.addEventListener('scroll', (event) => {
+    if (!ticking) {
+        window.requestAnimationFrame(() => {
+
+            ticking = false;
+        });
+
+
+        const rect = contentDiv.getBoundingClientRect();
+        const maxY = rect.height - rect.top;
+        const progress = Math.min(2, Math.floor(3 * event.target.scrollTop / maxY));
+
+        clickTabbar(tabs[progress], tabTitles[progress], false);
+
+        ticking = true;
+    }
+
+});
